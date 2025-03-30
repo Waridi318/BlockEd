9f3f6d25fe2e05c436a7f77a7753979d3ed2563e
Waridi318 <rosenjeri7151@gmail.com>
2025-03-30 15:46:09 +0300

    Added the dist folder

--- frontend/blocked/vite.config.ts
+++ frontend/blocked/vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
-    tailwindcss(),
+    tailwindcss()
   ],
-})
+ build: {
+    outDir: 'dist', // This is the default output directory
+  }
+});
